6e0525edf22d1ec73091143b9c6cb5c3276d924f
TheIndra55 <theindra@protonmail.com>
2025-03-29 17:39:59 +0100

    Add tests

--- tests/ArrayTest.php
+++ tests/ArrayTest.php
@@ -1,7 +1,23 @@
 <?php
 
+use Astylodon\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 
 class ArrayTest extends TestCase
 {
+    public function testValid()
+    {
+        $schema = Validator::array(Validator::string());
+
+        $this->assertTrue($schema->validate(["Hello", "World"]));
+        $this->assertTrue($schema->validate([]));
+    }
+
+    public function testInvalid()
+    {
+        $schema = Validator::array(Validator::string());
+
+        $this->assertFalse($schema->validate("Hello, World!"));
+        $this->assertFalse($schema->validate([1, 2]));
+    }
 }
--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -1,7 +1,17 @@
 <?php
 
+use Astylodon\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 
 class ObjectTest extends TestCase
 {
+    public function testValid()
+    {
+        $schema = Validator::object([
+            "username" => Validator::string(),
+            "password" => Validator::string()
+        ]);
+
+        $this->assertTrue($schema->validate(["username" => "Hello", "password" => "World"]));
+    }
 }
